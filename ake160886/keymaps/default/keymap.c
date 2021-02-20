c9edd08a4c74862e26310c43d756065880b66376
HughMungis <admin@HughMungis.com>
2021-02-20 13:04:30 -0500

    added QMK macro example

--- ake160886/keymaps/default/keymap.c
+++ ake160886/keymaps/default/keymap.c
@@ -64,3 +64,34 @@ bool led_update_kb(led_t led_state) {
     return led_update_user(led_state);
 }
 */
+
+
+//lol nice
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case QMKBEST:
+        if (record->event.pressed) {
+            // when keycode QMKBEST is pressed
+            SEND_STRING("QMK is the best thing ever!");
+        } else {
+            // when keycode QMKBEST is released
+        }
+        break;
+
+    case QMKURL:
+        if (record->event.pressed) {
+            // when keycode QMKURL is pressed
+            SEND_STRING("https://qmk.fm/\n");
+        } else {
+            // when keycode QMKURL is released
+        }
+        break;
+
+    case MY_OTHER_MACRO:
+        if (record->event.pressed) {
+           SEND_STRING(SS_LCTL("ac")); // selects all and copies
+        }
+        break;
+    }
+    return true;
+};
