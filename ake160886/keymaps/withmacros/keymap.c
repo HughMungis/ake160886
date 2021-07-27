dd6a5f25d4b4cfa9c8f62f6810250d3b854c65db
Hugh Mungis <66029505+HughMungis@users.noreply.github.com>
2021-07-27 12:50:15 -0400

    Update keymap.c
    updated macro names

--- ake160886/keymaps/withmacros/keymap.c
+++ ake160886/keymaps/withmacros/keymap.c
@@ -58,12 +58,12 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[] [MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( \
     KC_MSEL,        KC_MPLY,KC_MUTE,  KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,  KC_F7,KC_F8,KC_F9,KC_F10,	  KC_F11,KC_F12,KC_NO,KC_NO,						KC_BRIU,   KC_NO,   KC_NO,   	 KC_NO,   KC_NO,   KC_NO,KC_WAKE,
-    KC_MPRV,KC_MNXT,KC_VOLD,KC_VOLU,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9, KC_0, 	  KC_NO,    KC_NO,    KC_NO,KC_NO,  					KC_BRID,   KC_NO,   KC_NO,   	 KC_PWR,BTC, KC_NO,KC_SLEP,
+    KC_MPRV,KC_MNXT,KC_VOLD,KC_VOLU,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9, KC_0, 	  KC_NO,    KC_NO,    KC_NO,KC_NO,  					KC_BRID,   KC_NO,   KC_NO,   	 KC_PWR,  KC_BTC,  KC_NO,KC_SLEP,
 
-    AF1,    AF2,    AF3,    AF4,      KC_ESC,  COPY, PSTE,   KC_HOME,KC_END, KC_PGUP, KC_PGDOWN,   KC_NO,   KC_NO,   KC_NO, 							KC_WHOM,KC_WSCH,      	 	 KC_NLCK, KC_NO,	     KC_NO,   	KC_PSLS,
+    AF1,    AF2,    AF3,    AF4,      KC_ESC,  KC_COPY, KC_PSTE, KC_HOME, KC_END,  KC_PGUP, KC_PGDOWN,   KC_NO,   KC_NO,   KC_NO, 							KC_WHOM,KC_WSCH,      	 	 KC_NLCK, KC_NO,	     KC_NO,   	KC_PSLS,
     AF5,    AF6,    AF7,    AF8,      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NO,  		KC_WBAK,KC_UP,KC_WFWD,	 	 KC_KP_7, KC_KP_8,   KC_KP_9,   KC_KP_ASTERISK,
     AF9,    AF10,   AF11,   AF12,     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,KC_ENT, 		KC_LEFT,KC_DOWN,KC_RGHT, 	 KC_KP_4, KC_KP_5,   KC_KP_6,   KC_KP_MINUS,
-    NMAP,   DIRB,   KC_NO,  FNCY,     KC_LSFT, KC_NO ,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,KC_GRAVE,KC_RSFT,  		KC_NO, 	KC_WFAV,KC_NO,	 	 KC_KP_1, KC_KP_2,   KC_KP_3,   KC_KP_PLUS,
+    KC_NMAP,KC_DIRB,KC_NO,  KC_FNCY,     KC_LSFT, KC_NO ,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,KC_GRAVE,KC_RSFT,  		KC_NO, 	KC_WFAV,KC_NO,	 	 KC_KP_1, KC_KP_2,   KC_KP_3,   KC_KP_PLUS,
     KC_NO,     KC_NO,    KC_NO,   	      KC_LCTL, KC_LWIN,      LALT,            SPC,        RALT, KC_RWIN,   KC_NO,  RCTL,     						KC_NO,   	KC_NO,   		 KC_KP_0,   KC_NO,   KC_NO
   )};
 
@@ -102,53 +102,52 @@ bool led_update_kb(led_t led_state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case FSH:
+    case KC_FSH:
         if (record->event.pressed) {
             // when keycode FSH is pressed it will type out the lyrics to the Fresh Prince song
             SEND_STRING("Now this is a story all about how\n My life got flipped turned upside down\n And I'd like to take a minute, just sit right there\n I'll tell you how I became the prince of a town called Bel-Air\n In West Philadelphia born and raised\n On the playground is where I spent most of my days\n Chilling out, maxing, relaxing all cool\n And all shooting some b-ball outside of the school\n When a couple of guys who were up to no good\n Started making trouble in my neighborhood\n I got in one little fight and my mom got scared\n And said \"You're moving with your auntie and uncle in Bel-Air\" I begged and pleaded with her day after day");
         }
         break;
 
-    case FNCY:
+    case KC_FNCY:
         if (record->event.pressed) {
             // when keycode FNCY is pressed
             SEND_STRING("python -c 'import pty;pty.spawn(\"/bin/bash\");'\n" SS_RCTL("z") "stty raw -echo\n");
         }
         break;
 
-    case BTC:
+    case KC_BTC:
         if (record->event.pressed) {
             // when keycode BTC is pressed
             SEND_STRING("1EHmij4VnP3j8pFfi4AksnG7RYUyBMdrEF");
         }
         break;
 
-    case NMAP:
+    case KC_NMAP:
         if (record->event.pressed) {
             // when keycode BTC is pressed
             SEND_STRING("nmap -v -sV -O -sC -oN first.txt ");
         }
         break;
         
-    case DIRB:
+    case KC_DIRB:
         if (record->event.pressed) {
             // when keycode DIRB is pressed
             SEND_STRING("dirb URL_HERE /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o dirb.txt -w -v");
         }
         break;
         
-    case COPY:
+    case KC_COPY:
         if (record->event.pressed) {
-           SEND_STRING(SS_LCTL(KC_INS)); // ctrl + ins
+           SEND_STRING(SS_LCTL(SS_TAP(X_INS))); // ctrl + ins
         }
         break;
 
-    case PSTE:
+    case KC_PSTE:
         if (record->event.pressed) {
-           SEND_STRING(SS_LSFT(KC_INS)); // shift + ins
+           SEND_STRING(SS_LSFT(SS_TAP(X_INS))); // shift + ins
         }
         break;
     }
     return true;
 };
-
