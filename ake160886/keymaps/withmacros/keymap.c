0aa39de3a1b9d7813368c36b013d61dfa454f90c
Hugh Mungis <66029505+HughMungis@users.noreply.github.com>
2021-07-27 12:00:40 -0400

    Update keymap.c

--- ake160886/keymaps/withmacros/keymap.c
+++ ake160886/keymaps/withmacros/keymap.c
@@ -45,16 +45,26 @@
 // This is me trying to make a normal-ish layout in the middle of the keyboard, leaving the rest unassigned
 // Going to need a wiiiide editor window for this to look readable
 // Available keycodes are at https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes.md
+enum custom_keycodes {
+    FSH = SAFE_RANGE,
+    FNCY,
+    BTC,
+    NMAP,
+    DIRB,
+    COPY,
+    PSTE
+};
+
 const uint16_t PROGMEM keymaps[] [MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( \
-    KC_MSEL,        KC_MPLY,KC_MUTE,  KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,  KC_F7,KC_F8,KC_F9,KC_F10,	  KC_F11,KC_F12,NO,NO,	KC_BRIU,   NO,   NO,   	 NO,   NO,   NO,KC_WAKE,
-    KC_MPRV,KC_MNXT,KC_VOLD,KC_VOLU,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9, KC_0, 	   NO,    NO,    NO,NO, KC_BRID,   NO,   NO,   	 KC_PWR,BTC, NO,KC_SLEP,
+    KC_MSEL,        KC_MPLY,KC_MUTE,  KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,  KC_F7,KC_F8,KC_F9,KC_F10,	  KC_F11,KC_F12,NO,NO,						KC_BRIU,   NO,   NO,   	 NO,   NO,   NO,KC_WAKE,
+    KC_MPRV,KC_MNXT,KC_VOLD,KC_VOLU,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9, KC_0, 	  NO,    NO,    NO,NO,  					KC_BRID,   NO,   NO,   	 KC_PWR,BTC, NO,KC_SLEP,
 
-    AF1,    AF2,    AF3,    AF4,      ESC,  COPY, PSTE,   KC_HOME,KC_END, KC_PGUP, KC_PGDOWN,   NO,   NO,   NO, 		            KC_WHOM,KC_WSCH,      	 KC_NLCK,   NO,   NO,   KC_PSLS,
-    AF5,    AF6,    AF7,    AF8,      KC_TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    LBRC, RBRC, NO,   	KC_WBAK,KC_UP,KC_WFWD,	 KC_KP_7,   KC_KP_8,   KC_KP_9,   KC_KP_ASTERISK,
-    AF9,    AF10,   AF11,   AF12,     KC_CAPS, A,    S,    D,    F,    G,    H,    J,    K,    L,KC_SCLN, KC_QUOT, NO,KC_ENT, KC_LEFT,KC_DOWN,KC_RGHT,KC_KP_4,   KC_KP_5,   KC_KP_6,   KC_KP_MINUS,
-    NMAP,   DIRB,   NO,     FNCY,     KC_LSFT, NO ,Z,X,  C,    V,    B,    N,M,KC_COMM,KC_DOT,KC_SLSH,KC_GRAVE,KC_RSFT,       NO,   KC_WFAV,	KC_KP_1, KC_KP_2,   KC_KP_3,   KC_KP_PLUS,
-    NO,     NO,    NO,   	            KC_LCTL, KC_LWIN,      LALT,            SPC,        RALT, KC_RWIN,   NO,  RCTL, 		      NO,   NO,   		          KC_KP_0,   NO,   NO,
+    AF1,    AF2,    AF3,    AF4,      KC_ESC,  COPY, PSTE,   KC_HOME,KC_END, KC_PGUP, KC_PGDOWN,   NO,   NO,   NO, 							KC_WHOM,KC_WSCH,      	 KC_NLCK, NO,	     NO,   	KC_PSLS,
+    AF5,    AF6,    AF7,    AF8,      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, NO,  	KC_WBAK,KC_UP,KC_WFWD,	 KC_KP_7, KC_KP_8,   KC_KP_9,   KC_KP_ASTERISK,
+    AF9,    AF10,   AF11,   AF12,     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, NO,KC_ENT, 		KC_LEFT,KC_DOWN,KC_RGHT, KC_KP_4, KC_KP_5,   KC_KP_6,   KC_KP_MINUS,
+    NMAP,   DIRB,   NO,     FNCY,     KC_LSFT, NO ,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,KC_GRAVE,KC_RSFT,  	NO, 	KC_WFAV,NO,	 KC_KP_1, KC_KP_2,   KC_KP_3,   KC_KP_PLUS,
+    NO,     NO,    NO,   	      KC_LCTL, KC_LWIN,      LALT,            SPC,        RALT, KC_RWIN,   NO,  RCTL,     						NO,   	NO,   		 KC_KP_0,   NO,   NO
   )};
 
 // Optional override functions below.
@@ -89,15 +99,6 @@ bool led_update_kb(led_t led_state) {
 }
 */
 
-enum custom_keycodes {
-    FSH = SAFE_RANGE,
-    FNCY,
-    BTC,
-    NMAP,
-    DIRB,
-    COPY,
-    PSTE
-};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
