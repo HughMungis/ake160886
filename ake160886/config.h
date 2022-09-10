0980367c1aa9c20008fc095fe0e9bf85851e92ed
Hugh Mungis <66029505+HughMungis@users.noreply.github.com>
2022-09-10 13:05:41 -0400

    Update config.h

--- ake160886/config.h
+++ ake160886/config.h
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+//#define MATRIX_ROW_PINS { D0, D5 }
+//#define MATRIX_COL_PINS { F1, F0, B0 }
 //#define UNUSED_PINS #deprecated
 
 /* COL2ROW, ROW2COL*/
